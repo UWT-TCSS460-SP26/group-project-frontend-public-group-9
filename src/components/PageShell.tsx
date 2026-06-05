ba0a441541f8f10aede9eeae1c779595b693a63b
RaidenH-UWT <raidenh@uw.edu>
2026-06-05 14:20:03 -0700

    Add skip to content and scroll to top buttons

--- src/components/PageShell.tsx
+++ src/components/PageShell.tsx
@@ -2,6 +2,7 @@
 
 import { UserBadge } from '@/components/AuthButtons';
 import DarkModeToggle from '@/components/DarkModeToggle';
+import ScrollToTop from '@/components/ScrollToTop';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faStar } from '@fortawesome/free-regular-svg-icons';
@@ -16,6 +17,8 @@ export default function PageShell({ children }: { children: ReactNode }) {
 
     return (
         <section>
+            <Link href="#content" className="not-focus:sr-only focus:block absolute top-2 left-2 text-lg bg-background-less p-2 rounded">Skip to main content</Link>
+            <ScrollToTop />
             <header className="flex items-center justify-between w-full max-w-5xl mx-auto px-6 py-2 border-b border-background-less text-foreground">
                 <Link href="/" className="px-3 py-3 text-lg font-medium hover:text-foreground-less hover:bg-background-less">
                     <FontAwesomeIcon icon={faHouse} />
@@ -36,7 +39,7 @@ export default function PageShell({ children }: { children: ReactNode }) {
                 <UserBadge />
                 <DarkModeToggle />
             </header>
-            <main className="flex flex-col items-center justify-center flex-1 max-w-5xl mx-auto my-5 bg-background text-foreground font-sans">
+            <main id="content" className="flex flex-col items-center justify-center flex-1 max-w-5xl mx-auto my-5 bg-background text-foreground font-sans">
                 { children }
             </main>
         </section>
